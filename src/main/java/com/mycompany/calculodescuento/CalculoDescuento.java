ac57a119ae09a56b5422e7c8fdb159a8c8a2fc4d
Adrian Carvajal <carvajaladrian4cts@gmail.com>
2025-09-21 20:57:18 -0500

    Calculo de descuento

--- src/main/java/com/mycompany/calculodescuento/CalculoDescuento.java
+++ src/main/java/com/mycompany/calculodescuento/CalculoDescuento.java
@@ -13,13 +13,15 @@ public class CalculoDescuento {
     public static void main(String[] args) {
         double monto1 = 200.0;
         double monto2 = 500.0;
-
+        // Llamada al metodo calcularDescuentousando, descuento por defecto (10%)
         double descuento1 = calcularDescuento(monto1);
         double totalPagar1 = monto1 - descuento1;
 
+        // Llamada al metodo calcularDescuentousando, usando un descuento específico (15%)
         double descuento2 = calcularDescuento(monto2, 15.0);
         double totalPagar2 = monto2 - descuento2;
-
+        
+        // Resultados en consola
         System.out.println("---- Calculo de Descuentos ----");
         System.out.println("Monto: $" + monto1);
         System.out.println("Descuento aplicado (10%): $" + descuento1);
@@ -30,10 +32,12 @@ public class CalculoDescuento {
         System.out.println("Total a pagar: $" + totalPagar2);
     }
     
+    // Metodo que calcula el descuento con porcentaje definido al llamar el metodo
     public static double calcularDescuento(double montoTotal, double porcentajeDescuento) {
         return montoTotal * (porcentajeDescuento / 100);
     }
 
+    // Metodo que calcula el descuento con porcentaje de 10%
     public static double calcularDescuento(double montoTotal) {
         return montoTotal * 0.10;
     }
